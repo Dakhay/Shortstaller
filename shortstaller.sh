06f75b99f254f87237af55a802a88269cebd662a
Phewbility <matthieu.dipaola@icloud.com>
2024-02-27 15:08:16 +0100

    cargo/ruby

--- shortstaller.sh
+++ shortstaller.sh
@@ -2,9 +2,13 @@
 
 if [ "$(id -u)" -eq 0 ]; then 
     apt install nala -y
-    nala install exa gdb curl wget gdb docker -y 
+    nala install exa gdb curl wget gdb docker terminator bat nano iproute2 nmap snap cargo ruby  -y 
     nala update 
     nala upgrade -y
+
+    #Metasploit 
+    snap install metasploit-framework
+
 
     #EZA Install
     wget -c https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz -O - | tar xz
@@ -25,15 +29,12 @@ if [ "$(id -u)" -eq 0 ]; then
     echo "alias upg='sudo nala upgrade'"  >> ~/.bash_aliases  
     echo "alias maj='upd && upg -y'" >> ~/.bash_aliases 
     echo "alias cl='clear'" >> ~/.bash_aliases
+    echo "alias bat='batcat'" >> ~/.bash_aliases
+    echo "alias cat='batcat'" >> ~/.bash_aliases
     echo "alias cping='ping google.fr'" >> ~/.bash_aliases
 
-    source ~/.bash_aliases
+    echo "source ~/.bash_aliases" > /dev/null
 
-
-    
-
-
-    
 
 else
   echo "Run this script has root."
